be327fb741a9dd1376fe4847751b41fc1fd64d38
Kate <katepdonahue@gmail.com>
2014-03-11 18:01:45 -0400

    Add basic post info to actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,18 +1,23 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(params[:post])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
