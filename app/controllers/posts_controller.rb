a236e624aa289012c5ef5bc003acd8e88bd538b6
Kate <katepdonahue@gmail.com>
2014-03-11 18:16:05 -0400

    Add rails 4 verison of attr_accessibles

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,4 +26,12 @@ class PostsController < ApplicationController
   def destroy
   end
 
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def comment_params
+    params.require(:comment).permit(:post_id, :body)
+  end
+
 end
