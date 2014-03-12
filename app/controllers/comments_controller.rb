98b57f6c964287340a5dde3a40ed501a6ce18a28
Kate <katepdonahue@gmail.com>
2014-03-12 08:53:39 -0400

    Add new comment partial

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,21 +1,30 @@
 class CommentsController < ApplicationController
 
   def index
+    @comments = Comment.all
   end
 
   def show
+    @comment = Comment.find(params[:id])
   end
 
   def new
+    @comment = Comment.new
   end
 
   def create
+    @comment = Comment.create(params[:comment])
   end
 
   def edit
+    @comment = Comment.find(params[:id])
+    redirect_to :post
   end
 
   def update
+    @comment = Comment.find(params[:id])
+    @comment.update(params[:comment])
+    redirect_to :post
   end
 
   def destroy
